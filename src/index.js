f22985bd7642d9d2d59be909914212fd27fae36b
Julia Moskvita <ymoskvita@gmail.com>
2021-11-11 12:08:36 +0200

    update

--- src/index.js
+++ src/index.js
@@ -16,22 +16,10 @@ const loadMoreBtn = new LoadMoreBtn({
   hidden: true,
 });
 const imagesApiService = new ImagesApiService();
-// const observer = new IntersectionObserver(observerHandler, { threshold: 1, });
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.articlesContainer.addEventListener('click', onImgClick);
 loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
-
-// observer.observe(refs.anchor);
-
-// function observerHandler(entry, observer) {
-//   console.log(imagesApiService.page);
-//   if (imagesApiService.page >= 2) {
-//     // if (!entry.isIntersecting) return;
-//     fetchArticles();
-//   }
-//   // observer.unobserve(refs.anchor);
-// }
 
 function onSearch(e) {
   e.preventDefault();
@@ -64,6 +52,11 @@ loadMoreBtn.disabled();
    appendArticlesMarkup(articles);
     loadMoreBtn.enable();
 
+    if (!articles.length) {
+      loadMoreBtn.hide();
+      return Notiflix.Notify.warning('Pictures are over');
+    }
+
     refs.anchor.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
@@ -71,20 +64,6 @@ loadMoreBtn.disabled();
   })
 
 }
-
-// function fetchArticles() {
-//   loadMoreBtn.disabled();
-
-//   imagesApiService.fetchArticles().then(articles => {
-//    appendArticlesMarkup(articles);
-//     loadMoreBtn.enable();
-
-//     refs.anchor.scrollIntoView({
-//     behavior: 'smooth',
-//     block: 'end',
-//   });
-//   })
-// }
 
 function appendArticlesMarkup(articles) {
   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,20 +1,10 @@
 const URL = 'https://pixabay.com/api/';
 const apiKey = '24148386-011d38fdfa24550f9a52c4ec8';
-
 export default class ImagesApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
-
-  // fetchArticles() {
-  //   return fetch(`${URL}?key=${apiKey}&q=${this.searchQuery}&page=${this.page}&per_page=12&image_type=photo&pretty=true&orientation=horizontal`)
-  //     .then(response => response.json())
-  //     .then(({ hits }) => {
-  //       this.page += 1;
-  //       return hits;
-  //     })
-  // }
 
   async fetchArticles() {
     try {
